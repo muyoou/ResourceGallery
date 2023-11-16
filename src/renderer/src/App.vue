54b648449161e4cac6835c1a3b457583518e5db6
muyoo <44095535+muyoou@users.noreply.github.com>
2023-11-16 15:49:49 +0800

    更改ui

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,18 +1,51 @@
 <script setup lang="ts">
-import 'bootstrap';
-import './assets/css/bootstrap.min.css'
 import sideNav from './components/sideNav.vue'
 import setting from './components/setting.vue'
 import popBox from './components/popBox.vue'
+import {
+  HomeOutlined,
+  ReadOutlined,
+  LaptopOutlined,
+  PictureOutlined,
+  SettingOutlined,
+} from '@ant-design/icons-vue';
+import { ref } from 'vue';
+const collapsed = ref<boolean>(false);
+const selectedKeys = ref<string[]>(['1']);
 </script>
 
 <template>
-  <sideNav></sideNav>
-  <setting></setting>
-  <popBox></popBox>
+  <a-layout style="min-height: 100vh">
+    <a-layout-sider v-model:collapsed="collapsed" collapsible>
+      <div class="logo" />
+      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+        <a-menu-item key="1">
+          <home-outlined />
+          <span>主页</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <ReadOutlined />
+          <span>漫画</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <LaptopOutlined />
+          <span>游戏</span>
+        </a-menu-item>
+        <a-menu-item key="4">
+          <PictureOutlined />
+          <span>图片</span>
+        </a-menu-item>
+        <a-menu-item key="5">
+          <SettingOutlined />
+          <span>设置</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <setting></setting>
+    </a-layout>
+  </a-layout>
 </template>
-
-
 <style lang="less">
 @import './assets/css/styles.less';
 </style>
--- src/renderer/src/components/setting.vue
+++ src/renderer/src/components/setting.vue
@@ -1,23 +1,65 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import {
+  ToolOutlined,
+  TagsOutlined,
+  SearchOutlined,
+} from '@ant-design/icons-vue';
+const selectedKeys2 = ref<string[]>(['1']);
+const openKeys = ref<string[]>(['sub1']);
 </script>
 
 <template>
-  <article class="right-box">
-    <div class="settingNav">
-      
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">常规</li>
-        <li class="list-group-item">分类</li>
-        <li class="list-group-item">识别</li>
-        <li class="list-group-item">外观</li>
-      </ul>
-    </div>
-    <div class="settingCon">
-      <h2>识别设置</h2>
-      <p class="infoText">以下设置用来在分析本地文件夹时，对每一个文件夹进行自动分类的设置。分类时将按照以下的条件将文件夹分为漫画、游戏、视频三类</p>
-      <hr>
-      <h4>漫画分类</h4>
-      <p class="infoText">以下为将一个文件夹判定为包含漫画的文件夹的自定义条件</p>
-    </div>
-  </article>
+      <a-layout style="padding: 24px 0; background: #fff">
+        <a-layout-sider width="200" style="background: #fff">
+          <a-menu
+            v-model:selectedKeys="selectedKeys2"
+            v-model:openKeys="openKeys"
+            mode="inline"
+            style="height: 100%"
+          >
+            <a-sub-menu key="sub1">
+              <template #title>
+                <span>
+                  <ToolOutlined />
+                  常规
+                </span>
+              </template>
+              <a-menu-item key="1">option1</a-menu-item>
+              <a-menu-item key="2">option2</a-menu-item>
+              <a-menu-item key="3">option3</a-menu-item>
+              <a-menu-item key="4">option4</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub2">
+              <template #title>
+                <span>
+                  <TagsOutlined />
+                  分类
+                </span>
+              </template>
+              <a-menu-item key="5">漫画分类</a-menu-item>
+              <a-menu-item key="6">游戏分类</a-menu-item>
+              <a-menu-item key="7">图片分类</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub3">
+              <template #title>
+                <span>
+                  <SearchOutlined />
+                  识别
+                </span>
+              </template>
+              <a-menu-item key="9">漫画识别</a-menu-item>
+              <a-menu-item key="10">游戏识别</a-menu-item>
+              <a-menu-item key="11">图片识别</a-menu-item>
+            </a-sub-menu>
+          </a-menu>
+        </a-layout-sider>
+        <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
+          <a-typography-title :level="2">识别设置</a-typography-title>
+      <a-typography-text type="secondary">以下设置用来在分析本地文件夹时，对每一个文件夹进行自动分类的设置。分类时将按照以下的条件将文件夹分为漫画、游戏、视频三类</a-typography-text>
+      <a-divider />
+      <a-typography-title :level="3">漫画分类</a-typography-title>
+      <a-typography-text type="secondary">以下为将一个文件夹判定为包含漫画的文件夹的自定义条件</a-typography-text>
+        </a-layout-content>
+      </a-layout>
 </template>
